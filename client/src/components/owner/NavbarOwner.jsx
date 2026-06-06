411351aafbbe5a09f88a217b5b5d961e892e5bc4
Rohit Sohni <rohitsohni@Rohits-MacBook-Air.local>
2026-06-06 17:31:09 +0530

    Remove owner navbar branding

--- client/src/components/owner/NavbarOwner.jsx
+++ client/src/components/owner/NavbarOwner.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom';
 import { useAppContext } from '../../context/AppContext';
 
 const NavbarOwner = () => {
@@ -8,10 +6,7 @@ const NavbarOwner = () => {
     const {user} = useAppContext()
 
   return (
-    <div className='flex items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border-borderColor relative transition-all'>
-      <Link to='/'>
-        <img src={assets.logo} alt="" className="h-7"/>
-      </Link>
+    <div className='flex items-center justify-end px-6 md:px-10 py-4 text-gray-500 border-b border-borderColor relative transition-all'>
       <p>Welcome, {user?.name || "Owner"}</p>
     </div>
   )
